b2d53f733dc508634e0cd7ff624bf9884b613795
Matheus Barbosa Santos <matheus.bsa@hotmail.com>
2023-10-19 15:01:15 -0300

    Adds dialog box to generate CSR

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("Olá, mundo!");
+document.getElementById('csr-button').onclick = function (event) {
+    event.preventDefault();
+    var csrDialog = document.getElementById('csr-dialog');
+    csrDialog.showModal();
+}
+
+document.getElementById('export-button').onclick = function (event) {
+    event.preventDefault();
+    var exportDialog = document.getElementById('export-dialog');
+    exportDialog.showModal();
+}
+
+// Botão de fechar a caixa de diálogo de CSR
+document.getElementById('close-dialog').onclick = function () {
+    var csrDialog = document.getElementById('csr-dialog');
+    csrDialog.close();
+}
+
+// Botão de fechar a caixa de diálogo de Exportar
+document.getElementById('close-export-dialog').onclick = function () {
+    var exportDialog = document.getElementById('export-dialog');
+    exportDialog.close();
+}
+
+window.onclick = function (event) {
+    if (event.target.className === 'dialog') {
+        event.target.close();
+    }
+}
