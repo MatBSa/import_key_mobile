95d4f1a227d145a10aa204f6d5c404fbd18700d6
Matheus Barbosa Santos <matheus.bsa@hotmail.com>
2023-10-23 15:10:09 -0300

    Add button to generate asymmetric keys

--- js/main.js
+++ js/main.js
@@ -10,13 +10,11 @@ document.getElementById('export-button').onclick = function (event) {
     exportDialog.showModal();
 }
 
-// Botão de fechar a caixa de diálogo de CSR
 document.getElementById('close-dialog').onclick = function () {
     var csrDialog = document.getElementById('csr-dialog');
     csrDialog.close();
 }
 
-// Botão de fechar a caixa de diálogo de Exportar
 document.getElementById('close-export-dialog').onclick = function () {
     var exportDialog = document.getElementById('export-dialog');
     exportDialog.close();
@@ -26,4 +24,31 @@ window.onclick = function (event) {
     if (event.target.className === 'dialog') {
         event.target.close();
     }
+}
+
+async function generateAsymmetricKey() {
+    try {
+        const keyPair = await window.crypto.subtle.generateKey(
+            {
+                name: "RSA-OAEP",
+                modulusLength: 2048,
+                publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+                hash: "SHA-256",
+            },
+            true,
+            ["encrypt", "decrypt"]
+        );
+
+        const keyList = document.getElementById("key-list");
+        const keyListItem = document.createElement("li");
+        keyListItem.textContent = "Chave gerada: " + new Date().toLocaleString();
+        keyList.appendChild(keyListItem);
+    } catch (error) {
+        console.error("Erro ao gerar a chave:", error);
+    }
+}
+
+document.getElementById('generate-key-button').onclick = function (event) {
+    event.preventDefault();
+    generateAsymmetricKey();
 }
